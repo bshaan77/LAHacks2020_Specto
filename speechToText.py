26ac80d1f6313850601432ec76b5849218ff28c0
IshaanBansal <ishaanbansal1001@gmail.com>
2020-03-28 16:04:16 -0700

    Update all

--- speechToText.py
+++ speechToText.py
@@ -5,29 +5,37 @@ import sys
 import datetime
 
 def start():
-    print("Respond New to enter into a new document, or  Respond Find to look up a previous conversation.")
-    userstatus = input()
-    userstatus = userstatus.lower()
-    if userstatus.stratswith(n):
-        print("Great! Lets start a new conversation")
-        print('Can I get your name')
-        name = input()
-        print(name + " What would you like to call the conversation")
-        convoName = input()
-        convoName = str(convoName)
-        convoName = (convoName + ".txt")
-    elif userstatus.startswith(f):
-        print("Great! Lets find the conversation")
-        print('Can I get your name')
-        name = input()
-        print(name + " What is the name of the conversation we are trying to find")
-        convoName = input()
-        convoName = str(convoName)
-        convoName = (convoName + ".txt")
-    else:
-        print("You didn't enter a valid response. Lets try that again")
+    print("What is your name?")
+    name = input()
+    print("Great! Lets get started with your conversation")
+    name = name.lower()
+    name = str(name)
+    filename = (name +".txt")
+    
+##    print("Respond New to enter into a new document, or  Respond Find to look up a previous conversation.")
+##    userstatus = input()
+##    userstatus = userstatus.lower()
+##    if userstatus.stratswith(n):
+##        print("Great! Lets start a new conversation")
+##        print('Can I get your name')
+##        name = input()
+##        print(name + " What would you like to call the conversation")
+##        convoName = input()
+##        convoName = str(convoName)
+##        convoName = (convoName + ".txt")
+##    elif userstatus.startswith(f):
+##        print("Great! Lets find the conversation")
+##        print('Can I get your name')
+##        name = input()
+##        print(name + " What is the name of the conversation we are trying to find")
+##        convoName = input()
+##        convoName = str(convoName)
+##        convoName = (convoName + ".txt")
+##    else:
+##        print("You didn't enter a valid response. Lets try that again")
+##        start()
         
-    print("If at any time you would like to leave, respond \"leave\"")
+##    print("If at any time you would like to leave, respond \"leave\"")
 ##    status = input()
 ##    status = status.lower()
 ##    if status.startswith(l):
@@ -41,7 +49,7 @@ def start():
             audio = r.listen(source)   
         try:
             print("You said " + r.recognize_google(audio))
-            database.insert('speechToText.txt', currentDT, r.recognize_google(audio))
+            database.insert(filename, currentDT, r.recognize_google(audio))
         except sr.UnknownValueError:
             print("Not understandable")
 
