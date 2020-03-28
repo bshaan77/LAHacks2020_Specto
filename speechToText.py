6d2e65762960939a4f2499a461eaac3457af3f75
IshaanBansal <ishaanbansal1001@gmail.com>
2020-03-28 15:17:02 -0700

    Update speechToText.py

--- speechToText.py
+++ speechToText.py
@@ -5,7 +5,28 @@ import sys
 import datetime
 
 def start():
-    print("Welcome to Specto")
+    print("Respond New to enter into a new document, or  Respond Find to look up a previous conversation.")
+    userstatus = input()
+    userstatus = userstatus.lower()
+    if userstatus.stratswith(n):
+        print("Great! Lets start a new conversation")
+        print('Can I get your name')
+        name = input()
+        print(name + " What would you like to call the conversation")
+        convoName = input()
+        convoName = str(convoName)
+        convoName = (convoName + ".txt")
+    elif userstatus.startswith(f):
+        print("Great! Lets find the conversation")
+        print('Can I get your name')
+        name = input()
+        print(name + " What is the name of the conversation we are trying to find")
+        convoName = input()
+        convoName = str(convoName)
+        convoName = (convoName + ".txt")
+    else:
+        print("You didn't enter a valid response. Lets try that again")
+        
     print("If at any time you would like to leave, respond \"leave\"")
 ##    status = input()
 ##    status = status.lower()
@@ -29,4 +50,5 @@ def leave():
     exit()
 
 while True:
+    print("Welcome to Specto")
     start()
